07cf1ed22253c1b503aa26c1d120e1cc18bd7162
hlaCk <somaxy@gmail.com>
2019-03-25 19:53:27 +0300

    Objects prototypes
    Change features functions to Object.definePrototype inested of OBJECT.protorype.
    
    prototypes not enumerable, configurable and writable anymore.

--- _z.js
+++ _z.js
@@ -5,58 +5,85 @@
 (function( window, document ) {
 // new Date().getUnixTime() = Unix timestamp
     if(typeof Date.prototype.getUnixTime !== 'function')
-        Date.prototype.getUnixTime = function() { return this.getTime()/1000|0; };
-
+		Object.defineProperty( Date.prototype, 'getUnixTime', {
+			value () { return this.getTime()/1000|0; },
+			
+            enumerable: false,
+            configurable: false,
+            writable: false
+		});
+		
 // Date.now() = js timestamp
     if(typeof Date.now !== 'function')
         Date.now = function() { return (new Date()).getTime(); };
-
+		
 // Date.time() = Unix timestamp
     if(typeof Date.time !== 'function')
         Date.time = function() { return new Date().getUnixTime(); };
-
+		
 // Function.callSelf(...arguments) = Function.apply( Function, arguments )
     if(typeof Function.prototype.callSelf !== 'function')
-        Function.prototype.callSelf = function() {
-            return this.apply( this, arguments );
-        };
+		Object.defineProperty( Function.prototype, 'callSelf', {
+			value () {
+				return this.apply( this, arguments );
+			},
+			
+            enumerable: false,
+            configurable: false,
+            writable: false
+		});
+		
 
 // Function.bindSelf(...arguments) = Function.bind( Function, arguments )
     if(typeof Function.prototype.bindSelf !== 'function')
-        Function.prototype.bindSelf = function( ) {
-            return this.bind( this, ...arguments );
-        };
+		Object.defineProperty( Function.prototype, 'bindSelf', {
+			value ( ) {
+				return this.bind( this, ...arguments );
+			},
+			
+            enumerable: false,
+            configurable: false,
+            writable: false
+		});
+		
 
 // Object.each(function) = Object
     if(typeof Object.prototype.each !== 'function')
-        Object.prototype.each = function( cb ) {
-            cb = cb || false;
-            if( !_z.isFunction(cb) ) return this;
+		Object.defineProperty( Object.prototype, 'each', {
+			value ( cb ) {
+				cb = cb || false;
+				if( !_z.isFunction(cb) ) return this;
 
-            if( _z&&_z["for"] ) {
-                var nO = _z.for(this, cb);
-                if( _z.isObject(nO) )
-                    Object.assign(this, nO);
-            } else {
-                try {
-                    var _keys = Object.keys( this );
-                    for( var i = 0, l = _keys.length; i < l ; i++ ) {
-                        var thisObj = {};
-                        thisObj[ _keys[i] ] = this[ _keys[i] ];
-                        var cbReturn = cb.apply(this,  [ _keys[i], this[ _keys[i] ], this[ _keys[i] ]] );
+				if( _z&&_z["for"] ) {
+					var nO = _z.for(this, cb);
+					if( _z.isObject(nO) )
+						Object.assign(this, nO);
+				} else {
+					try {
+						var _keys = Object.keys( this );
+						for( var i = 0, l = _keys.length; i < l ; i++ ) {
+							var thisObj = {};
+							thisObj[ _keys[i] ] = this[ _keys[i] ];
+							var cbReturn = cb.apply(this,  [ _keys[i], this[ _keys[i] ], this[ _keys[i] ]] );
 
-                        if( cbReturn === false )
-                            break;
-                        else if( cbReturn != undefined )
-                            this[ _keys[i] ] = cbReturn;
-                    }
-                } catch(e) {
-                    throw e;
-                }
-            }
+							if( cbReturn === false )
+								break;
+							else if( cbReturn != undefined )
+								this[ _keys[i] ] = cbReturn;
+						}
+					} catch(e) {
+						throw e;
+					}
+				}
 
-            return this;
-        };
+				return this;
+			},
+			
+            enumerable: false,
+            configurable: false,
+            writable: false
+		});
+		
 
 // Array.pushSetter='value' => Array.push( 'value' )
     if(typeof Array.prototype.pushSetter !== 'function')
@@ -64,25 +91,46 @@
 // todo: add in wiki
 // Object.getSize => size of object
     if(typeof Object.prototype.getSize !== 'function')
-        Object.prototype.getSize = function( o ) { o = o || this;
-            return _z.size(o);
-        };
+		Object.defineProperty( Object.prototype, 'getSize', {
+			value ( o ) { o = o || this;
+				return _z.size(o);
+			},
+			
+            enumerable: false,
+            configurable: false,
+            writable: false
+		});
+		
 
 // Object.getType => type of object lowerCase
     if(typeof Object.prototype.getType !== 'function')
-        Object.prototype.getType = function( toLowerCase ) { toLowerCase = toLowerCase || arguments.length ? false : true;
-            if(this instanceof _z) return "_z";
-            else if(this == _z) return "underz";
+		Object.defineProperty( Object.prototype, 'getType', {
+			value ( toLowerCase ) { toLowerCase = toLowerCase || arguments.length ? false : true;
+				if(this instanceof _z) return "_z";
+				else if(this == _z) return "underz";
 
-            return Object.prototype.toString.call( this ).replace("[object ", "").replace("]", "").trim()[toLowerCase ? "toLowerCase" : "trim"]();
-        };
+				return Object.prototype.toString.call( this ).replace("[object ", "").replace("]", "").trim()[toLowerCase ? "toLowerCase" : "trim"]();
+			},
+			
+            enumerable: false,
+            configurable: false,
+            writable: false
+		});
+		
 
 // Object.isType => true|false check object type
     if(typeof Object.prototype.isType !== 'function')
-        Object.prototype.isType = function( check ) {
-            check = ( arguments.length == 1 ) ? String(check).toLowerCase() :  -1;
-            return this.getType()==check;
-        };
+		Object.defineProperty( Object.prototype, 'isType', {
+			value ( check ) {
+				check = ( arguments.length == 1 ) ? String(check).toLowerCase() :  -1;
+				return this.getType()==check;
+			},
+			
+            enumerable: false,
+            configurable: false,
+            writable: false
+		});
+		
 
 // METHD1: Normal Array
     // [1, 2, 3, 1].unique() = [1, 2, 3]
@@ -101,152 +149,215 @@
     // a.push(b); // [ [ ID = 1 ], [ ID = 2 ], [ ID = 3 ], [ ID = 1 ] ]
     // a.unique("ID"); // [ [ ID = 1 ], [ ID = 2 ], [ ID = 3 ] ]
     if(typeof Array.prototype.unique !== 'function')
-        Array.prototype.unique = function(keyUnique){
-            var keyUnique = keyUnique || null;
+		Object.defineProperty( Array.prototype, 'unique', {
+			value (keyUnique){
+				var keyUnique = keyUnique || null;
 
-            if( keyUnique === null )
-                return [...new Set(this)];
+				if( keyUnique === null )
+					return [...new Set(this)];
 
-            var u = {}, a = [];
-            for(var i = 0, l = this.length; i < l; ++i){
-                var currentKeyElement = this[i];
-                currentKey = this[i][keyUnique];
+				var u = {}, a = [];
+				for(var i = 0, l = this.length; i < l; ++i){
+					var currentKeyElement = this[i];
+					currentKey = this[i][keyUnique];
 
-                if( u.hasOwnProperty(currentKey) ) continue;
+					if( u.hasOwnProperty(currentKey) ) continue;
 
-                a.push(currentKeyElement);
-                u[currentKey] = 1;
-            }
-            return a;
-        };
+					a.push(currentKeyElement);
+					u[currentKey] = 1;
+				}
+				return a;
+			},
+			
+            enumerable: false,
+            configurable: false,
+            writable: false
+		});
+		
 
 // Array.add( ...ARRAY ) = push all the arguments
     if(typeof Array.prototype.add !== 'function') {
-        Array.prototype.add = function() {
-            var arr = _z.Array(arguments) || [];
+		Object.defineProperty( Array.prototype, 'add', {
+			value () {
+				var arr = _z.Array(arguments) || [];
 
-            if( _z.isFunction(this.push) )
-                return this.push.apply(this, arr);
-            else
-                return _z.arrayAppend(this, ...arr);
-        };
+				if( _z.isFunction(this.push) )
+					return this.push.apply(this, arr);
+				else
+					return _z.arrayAppend(this, ...arr);
+			},
+			
+            enumerable: false,
+            configurable: false,
+            writable: false
+		});
+		
     }
 
 // Array.inArray(needle, haystack) = index OR -1 if not found
     if(typeof Array.prototype.inArray !== 'function')
-        Array.prototype.inArray = function(needle, haystack) {
-            var haystack = haystack || this;
-            if( !_z.isArray(haystack) ) return -1;
+		Object.defineProperty( Array.prototype, 'inArray', {
+			value (needle, haystack) {
+				var haystack = haystack || this;
+				if( !_z.isArray(haystack) ) return -1;
 
-            for(var i = 0, length = haystack.length; i < length; i++)
-                if(haystack[i] == needle)
-                    return haystack.indexOf(needle) || 0;
+				for(var i = 0, length = haystack.length; i < length; i++)
+					if(haystack[i] == needle)
+						return haystack.indexOf(needle) || 0;
 
-            return -1;
-        };
+				return -1;
+			},
+			
+            enumerable: false,
+            configurable: false,
+            writable: false
+		});
+		
 
 // Array.remove(from, to) = remove vars by index or value
     if(typeof Array.prototype.remove !== 'function')
-        Array.prototype.remove = function(from, to) {
-            var args = arguments;
-            if( args.length > 0 )
-                from = typeof(from)==typeof(7) ? from : this.indexOf( from );
+		Object.defineProperty( Array.prototype, 'remove', {
+			value (from, to) {
+				var args = arguments;
+				if( args.length > 0 )
+					from = typeof(from)==typeof(7) ? from : this.indexOf( from );
 
-            if( args.length > 1 )
-                to = typeof(to)==typeof(6) ? to : this.indexOf( to );
+				if( args.length > 1 )
+					to = typeof(to)==typeof(6) ? to : this.indexOf( to );
 
-            from = (from === -1 && args[0] !== -1) ? false : from;
-            to = (to === -1 && args[1] !== -1) ? false : to;
-            if( (!!!from && typeof(from)!=typeof(4)) && (!!!to && typeof(to)!=typeof(5)) )
-                return this;
+				from = (from === -1 && args[0] !== -1) ? false : from;
+				to = (to === -1 && args[1] !== -1) ? false : to;
+				if( (!!!from && typeof(from)!=typeof(4)) && (!!!to && typeof(to)!=typeof(5)) )
+					return this;
 
-            // Array Remove - By John Resig (MIT Licensed)
-            var rest = this.slice((to || from) + 1 || this.length);
-            this.length = from < 0 ? this.length + from : from;
+				// Array Remove - By John Resig (MIT Licensed)
+				var rest = this.slice((to || from) + 1 || this.length);
+				this.length = from < 0 ? this.length + from : from;
 
-            return this.push.apply(this, rest);
-        };
+				return this.push.apply(this, rest);
+			},
+			
+            enumerable: false,
+            configurable: false,
+            writable: false
+		});
+		
 
 // Array Array.removeAll(val) = remove vars by value
     if(typeof Array.prototype.removeAll !== 'function')
-        Array.prototype.removeAll = function(val) {
-            if( this.indexOf(val) === -1 )
-                return this;
+		Object.defineProperty( Array.prototype, 'removeAll', {
+			value (val) {
+				if( this.indexOf(val) === -1 )
+					return this;
 
-            while( this.indexOf(val) !== -1 && this.remove(val));
+				while( this.indexOf(val) !== -1 && this.remove(val));
 
-            return this;
-        };
+				return this;
+			},
+			
+            enumerable: false,
+            configurable: false,
+            writable: false
+		});
+		
 
 // String String.replaceArray(Array needle, Array haystack)
     if(typeof String.prototype.replaceArray !== 'function')
-        String.prototype.replaceArray = function(find, replace) {
-            var replaceString = this;
-            find = (find&&(find).isType("array")) ? find : [find] || [];
-            replace = (replace&&(replace).isType("array")) ? replace : [replace] || [];
+		Object.defineProperty( String.prototype, 'replaceArray', {
+			value (find, replace) {
+				var replaceString = this;
+				find = (find&&(find).isType("array")) ? find : [find] || [];
+				replace = (replace&&(replace).isType("array")) ? replace : [replace] || [];
 
-            for (var i = 0, fL = find.length; i < fL; i++)
-                replaceString = replaceString.replace(find[i], (replace&&(replace).isType("array")) ? replace[i]||"" : replace);
+				for (var i = 0, fL = find.length; i < fL; i++)
+					replaceString = replaceString.replace(find[i], (replace&&(replace).isType("array")) ? replace[i]||"" : replace);
 
-            return replaceString;
-        };
+				return replaceString;
+			},
+			
+            enumerable: false,
+            configurable: false,
+            writable: false
+		});
+		
 
 // String String.replaceAll(String needle, String haystack)
     if(typeof String.prototype.replaceAll !== 'function')
-        String.prototype.replaceAll = function(search, replacement) {
-            var target = this;
-            search = (search&&(search).isType("array")) ? search : [search] || [];
-            replacement = (replacement&&(replacement).isType("array")) ? replacement : [replacement] || [];
+		Object.defineProperty( String.prototype, 'replaceAll', {
+			value (search, replacement) {
+				var target = this;
+				search = (search&&(search).isType("array")) ? search : [search] || [];
+				replacement = (replacement&&(replacement).isType("array")) ? replacement : [replacement] || [];
 
-            if(search.length > 1) {
-                if( replacement.length < search.length )
-                    replacement.add( ...Array.from({length: search.length-replacement.length}, () => "") );
-                for (var i = 0, fL = search.length; i < fL; i++)
-                    target = target.split(search[i]).join((replacement&&(replacement).isType("array")) ? replacement[i] : replacement);
-                return target;
-            }
+				if(search.length > 1) {
+					if( replacement.length < search.length )
+						replacement.add( ...Array.from({length: search.length-replacement.length}, () => "") );
+					for (var i = 0, fL = search.length; i < fL; i++)
+						target = target.split(search[i]).join((replacement&&(replacement).isType("array")) ? replacement[i] : replacement);
+					return target;
+				}
 
-            search = search[0];
-            replacement = replacement.length ? replacement[0] : "";
-            replacement = (replacement===undefined || replacement===null) ? "" : replacement;
-            return target.split(search).join(replacement);
-        };
+				search = search[0];
+				replacement = replacement.length ? replacement[0] : "";
+				replacement = (replacement===undefined || replacement===null) ? "" : replacement;
+				return target.split(search).join(replacement);
+			},
+			
+            enumerable: false,
+            configurable: false,
+            writable: false
+		});
+		
 
 // String String.fromCode(int radix, String seprator, Boolean codeLen)
     if(typeof String.prototype.fromCode !== 'function')
-        String.prototype.fromCode = function codeToString( radix /*binary=2,octal=8,hex=16,code=undefined*/, separator/*""*/, codeLen/*false*/) {
-            radix = Number(radix) || undefined;
-            separator = separator || "";
-            var cLen = "H".charCodeAt(0).toString(radix).length;
-            codeLen = codeLen || ( codeLen===true ? 2 : Number(codeLen||0)||cLen );
+		Object.defineProperty( String.prototype, 'fromCode', {
+			value ( radix /*binary=2,octal=8,hex=16,code=undefined*/, separator/*""*/, codeLen/*false*/) {
+				radix = Number(radix) || undefined;
+				separator = separator || "";
+				var cLen = "H".charCodeAt(0).toString(radix).length;
+				codeLen = codeLen || ( codeLen===true ? 2 : Number(codeLen||0)||cLen );
 
-            var hexes = this;
-            if( separator ) hexes.replace(separator, "");
+				var hexes = this;
+				if( separator ) hexes.replace(separator, "");
 
-            hexes = hexes.match(new RegExp(".{1," + (codeLen||cLen) + "}", "g")) || [];
-            var back = "";
-            for(var j = 0, hL = hexes.length; j<hL; j++)
-                back += String.fromCharCode(parseInt(hexes[j], radix));
+				hexes = hexes.match(new RegExp(".{1," + (codeLen||cLen) + "}", "g")) || [];
+				var back = "";
+				for(var j = 0, hL = hexes.length; j<hL; j++)
+					back += String.fromCharCode(parseInt(hexes[j], radix));
 
-            return back;
-        };
+				return back;
+			},
+			
+            enumerable: false,
+            configurable: false,
+            writable: false
+		});
+		
 // todo: wiki+
 // String String.toCode(int radix, String seprator, Boolean codeLen)
     if(typeof String.prototype.toCode !== 'function')
-        String.prototype.toCode = function stringToCode( radix /*binary=2,octal=8,hex=16,code=undefined*/, separator/*""*/, codeLen/*false*/) {
-            radix = Number(radix) || undefined;
-            separator = separator || "";
-            var cLen = "H".charCodeAt(0).toString(radix).length;
-            codeLen = codeLen|| ( (codeLen===true ? 2 : Number(codeLen||0)||cLen) * (-1) );
+		Object.defineProperty( String.prototype, 'toCode', {
+			value ( radix /*binary=2,octal=8,hex=16,code=undefined*/, separator/*""*/, codeLen/*false*/) {
+				radix = Number(radix) || undefined;
+				separator = separator || "";
+				var cLen = "H".charCodeAt(0).toString(radix).length;
+				codeLen = codeLen|| ( (codeLen===true ? 2 : Number(codeLen||0)||cLen) * (-1) );
 
-            var hex;
-            try { hex = unescape(encodeURIComponent(this)).split('').map((v)=>{
-                    return ("000000"+v.charCodeAt(0).toString(radix)).slice( codeLen || cLen*(-1) );
-                }).join(separator||"");
-            } catch(e) { console.log('Invalid text input: ' + (hex = this)); }
+				var hex;
+				try { hex = unescape(encodeURIComponent(this)).split('').map((v)=>{
+						return ("000000"+v.charCodeAt(0).toString(radix)).slice( codeLen || cLen*(-1) );
+					}).join(separator||"");
+				} catch(e) { console.log('Invalid text input: ' + (hex = this)); }
 
-            return hex;
-        };
+				return hex;
+			},
+			
+            enumerable: false,
+            configurable: false,
+            writable: false
+		});
+		
 
 // RegExp.regexes = Regexes source
     if(typeof RegExp.regexes !== 'object') {
@@ -267,9 +378,16 @@
 
 // RegExp.setFlags(...flags) = add flags to the regex
     if(typeof RegExp.prototype.setFlags !== 'function')
-        RegExp.prototype.setFlags = function setFlags(...flags) {
-            return new RegExp(this.source, flags.join(''));
-        };
+		Object.defineProperty( RegExp.prototype, 'setFlags', {
+			value (...flags) {
+				return new RegExp(this.source, flags.join(''));
+			},
+			
+            enumerable: false,
+            configurable: false,
+            writable: false
+		});
+		
 
 // is it real document
 if( !("isdocument" in document) )
